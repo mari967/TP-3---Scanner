7d118c3fccca84f66380ed2d56ae9a5835f53238
mari967 <mar.y.sol.240@gmail.com>
2018-09-28 22:16:36 -0300

    Agregando datos del grupo al main

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+/*TP3  –  2018
+Trabajo práctco N° 3
+Grupo  10
+Cervantes Marisol, 1639201
+Lodeiros Ayrton, 1646473
+Nishisaka Matías, 
+Rey Gastσn, 1561674*/
+
+
+
 #include <stdio.h>
 #include "scanner.h"
 
