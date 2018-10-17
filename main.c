df914d7f84021f203eb7dd0f6238e051d436c6bb
mari967 <mar.y.sol.240@gmail.com>
2018-10-16 21:14:17 -0300

    Solicionado el error que daba una salida incorrecta cuando se retornaban los caracteres desde el scanner

---
+++ main.c
@@ -0,0 +1,29 @@
+/*TP3  –  2018
+Trabajo práctco N° 3
+Grupo  10
+Cervantes Marisol, 1639201
+Lodeiros Ayrton, 1646473
+Nishizaka Matías, 1634483
+Rey Gastσn, 1561674*/
+
+
+
+#include <stdio.h>
+#include <ctype.h>
+#include "scanner.h"
+
+char *token_names[] = {"Fin de archivo", "Asignación", "Constante", "Programa", "Variables", "Definir", "Código", "Leer", "Escribir", "Fin", "Identificador"};
+
+
+int main () {
+
+enum tokens t;
+while ( (t = yylex()) ) { 
+if (t == IDENTIFICADOR || t == CONSTANTE)
+printf("Token: %s\tlexema: %s\n", token_names[t], yytext);
+else if (t >= '(')
+	printf("Token:  '%c'\n",t);
+else printf("Token: %s\n", token_names[t]);
+	}
+printf("Token: %s", token_names[t]);
+}
